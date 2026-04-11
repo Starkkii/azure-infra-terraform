5e2e815cc90da3fdae8ee78ac81cb911cf247f67
Starkkii <villevaltteristarck@gmail.com>
2026-04-11 16:37:13 +0300

    Add App Service Plan and App Service to main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,22 @@ resource "azurerm_resource_group" "main" {
   name = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_service_plan" "main" {
+  name = "asp-harkka"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  os_type = "Windows"
+  sku_name = "F1"
+}
+
+resource "azurerm_windows_web_app" "main" {
+  name = "app-harkka-stara-vv"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  service_plan_id = azurerm_service_plan.main.id
+
+  site_config {
+    always_on = false
+  }
+}
